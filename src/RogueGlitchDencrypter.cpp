9d5fe991d522e93111d1886b4ddb8bd1dc5c7322
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2023-11-23 13:41:05 +0100

    adept dencryption to usecase

--- src/RogueGlitchDencrypter.cpp
+++ src/RogueGlitchDencrypter.cpp
@@ -13,26 +13,6 @@
 #include <cryptopp/pwdbased.h>
 #include <cryptopp/sha.h>
 
-/*
- public override void Decrypt(Stream input, Stream output, string password, int bufferSize)
-   {
-     using (Aes aes = Aes.Create())
-     {
-       byte[] buffer = new byte[16];
-       input.Read(buffer, 0, 16);
-       aes.IV = buffer;
-       Rfc2898DeriveBytes rfc2898DeriveBytes = new Rfc2898DeriveBytes(password, aes.IV, 100);
-       aes.Key = rfc2898DeriveBytes.GetBytes(16);
-       using (ICryptoTransform decryptor = aes.CreateDecryptor())
-       {
-         using (CryptoStream input1 = new CryptoStream(input, decryptor, CryptoStreamMode.Read))
-           EncryptionAlgorithm.CopyStream((Stream) input1, output, bufferSize);
-       }
-     }
-     output.Position = 0L;
-   }
- */
-
 using CryptoPP::byte;
 
 enum class Version
@@ -53,80 +33,32 @@ std::vector<byte> operator""_byte(const char* bytes, size_t size)
 std::map<Version, std::vector<byte>> esKeys{{Version::Ultra, "YouShouldNotBeAbleToReadThis_1337"_byte},
     {Version::Lagacy, "YouAreACheaterIfYouReadThis_376347"_byte}};
 
-int decrypt(std::filesystem::path inFile)
+std::string decrypt(unsigned char* data, unsigned size)
 {
-	for(const auto& [version, key] : esKeys)
-	{
-		try
-		{
-			std::ifstream inStream(inFile, std::ios_base::in | std::ios_base::binary);
-			if(!inStream.is_open())
-			{
-				std::cout << "Could not open input file!" << std::endl;
-				return -1;
-			}
+	std::array<byte, BUFSIZE> ivBuffer;
+	std::copy_n(data, ivBuffer.size(), ivBuffer.data());
 
-			std::array<byte, BUFSIZE> ivBuffer;
-			inStream.read((char*)ivBuffer.data(), ivBuffer.size());
+	std::array<byte, BUFSIZE> keyBuffer;
+	CryptoPP::PKCS5_PBKDF2_HMAC<CryptoPP::SHA1> deriver;
 
-			std::array<byte, BUFSIZE> keyBuffer;
-			CryptoPP::PKCS5_PBKDF2_HMAC<CryptoPP::SHA1> deriver;
+	deriver.DeriveKey(keyBuffer.data(), keyBuffer.size(), UNUSED, esKeys[Version::Lagacy].data(),
+	    esKeys[Version::Lagacy].size(), ivBuffer.data(), ivBuffer.size(), 100);
 
-			deriver.DeriveKey(keyBuffer.data(), keyBuffer.size(), UNUSED, key.data(), key.size(), ivBuffer.data(),
-			    ivBuffer.size(), 100);
+	CryptoPP::CBC_Mode<CryptoPP::AES>::Decryption decrypter;
 
-			CryptoPP::CBC_Mode<CryptoPP::AES>::Decryption decrypter;
+	decrypter.SetKeyWithIV(keyBuffer.data(), keyBuffer.size(), ivBuffer.data(), ivBuffer.size());
 
-			decrypter.SetKeyWithIV(keyBuffer.data(), keyBuffer.size(), ivBuffer.data(), ivBuffer.size());
+	std::string outString;
 
-			std::string outString;
+	CryptoPP::ArraySource fs(data + ivBuffer.size(), size - ivBuffer.size(), true,
+	    new CryptoPP::StreamTransformationFilter(decrypter, new CryptoPP::StringSink(outString)));
 
-			CryptoPP::FileSource fs(inStream, true,
-			    new CryptoPP::StreamTransformationFilter(decrypter, new CryptoPP::StringSink(outString)));
-
-			if(outString.front() != '{')
-			{
-				continue;
-			}
-
-			std::filesystem::path outFile(inFile);
-			outFile.replace_extension(".json");
-
-			std::ofstream outStream(outFile, std::ios_base::out | std::ios_base::binary);
-			if(!outStream.is_open())
-			{
-				std::cout << "Could not open output file!" << std::endl;
-				return -1;
-			}
-
-			outStream << outString;
-
-			return 0;
-		}
-		catch(...)
-		{
-		}
-	}
-
-	std::cout << "Could decrypt file!" << std::endl;
-	return -1;
+	return outString;
 }
 
-int encrypt(std::filesystem::path file)
+std::vector<unsigned char> encrypt(std::string inString)
 {
-	std::ifstream inStream(file, std::ios_base::in | std::ios_base::binary);
-	if(!inStream.is_open())
-	{
-		std::cout << "Could not open input file!" << std::endl;
-		return -1;
-	}
-
-	std::string inString{std::istreambuf_iterator<char>(inStream), std::istreambuf_iterator<char>()};
-
-	inStream.close();
-
-	const std::vector<byte>& password =
-	    inString.find("KablooeyChallenge") != std::string::npos ? esKeys[Version::Ultra] : esKeys[Version::Lagacy];
+	const std::vector<byte>& password = esKeys[Version::Ultra];
 
 	std::array<byte, BUFSIZE> ivBuffer;
 	CryptoPP::AutoSeededRandomPool rng;
@@ -142,19 +74,12 @@ int encrypt(std::filesystem::path file)
 
 	encrypter.SetKeyWithIV(keyBuffer.data(), keyBuffer.size(), ivBuffer.data(), ivBuffer.size());
 
-	file.replace_extension(".glitch");
+	std::vector<unsigned char> outVec;
 
-	std::ofstream outStream(file, std::ios_base::out | std::ios_base::binary);
-	if(!outStream.is_open())
-	{
-		std::cout << "Could not open output file!" << std::endl;
-		return -1;
-	}
-
-	outStream.write((char*)ivBuffer.data(), ivBuffer.size());
+	outVec.insert(outVec.begin(), ivBuffer.begin(), ivBuffer.end());
 
 	CryptoPP::StringSource fs(
-	    inString, true, new CryptoPP::StreamTransformationFilter(encrypter, new CryptoPP::FileSink(outStream)));
+	    inString, true, new CryptoPP::StreamTransformationFilter(encrypter, new CryptoPP::VectorSink(outVec)));
 
-	return 0;
+	return outVec;
 }
