3237351b2cf5a1a3b9abba33f9832b310d83bceb
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2023-11-25 13:43:35 +0100

    send file contents to c++

--- src/webui.cpp
+++ src/webui.cpp
@@ -7,10 +7,9 @@
 
 extern "C"
 {
-	EMSCRIPTEN_KEEPALIVE const char* processFile(const char* fileContent)
+	EMSCRIPTEN_KEEPALIVE bool tryUpgrade(const unsigned char* fileContent, unsigned size)
 	{
-		static std::string contentBuffer;
-		contentBuffer.assign("Modified: ").append(fileContent);
-		return contentBuffer.c_str();
+		emscripten_log(EM_LOG_INFO, "Size: %d, Content: %.60s", size, reinterpret_cast<const char*>(fileContent));
+		return fileContent && size;
 	}
 }
