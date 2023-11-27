414574e5243bfe356d91004f16d9fb6a59bff882
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2023-11-27 12:04:52 +0100

    remove submodules

--- ext/raygui.c
+++
@@ -1,5 +0,0 @@
-#define RAYGUI_IMPLEMENTATION
-#include "raygui/src/raygui.h"
-
-#define GUI_TEXTBOX_EXTENDED_IMPLEMENTATION
-#include "raygui/src/extras/gui_textbox_extended.h"
--- ext/rres.c
+++
@@ -1,8 +0,0 @@
-#include <raylib.h>
-
-#define RRES_IMPLEMENTATION
-#define RRES_RAYLIB_IMPLEMENTATION
-#define RRES_SUPPORT_COMPRESSION_LZ4
-#define RRES_SUPPORT_ENCRYPTION_AES
-#define RRES_SUPPORT_ENCRYPTION_XCHACHA20
-#include "rres/src/rres-raylib.h"
