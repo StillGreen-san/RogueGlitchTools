08eda04bc4a5a60637b5c48e4ef754e8e81e8312
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2023-11-27 13:18:41 +0100

    update readme & changelog

--- inc/dependency_info.hpp
+++ inc/dependency_info.hpp
@@ -11,17 +11,14 @@ struct LibraryInfo
 	std::string_view url;
 	std::string_view lic;
 };
-constexpr LibraryInfo PROJECT_INFO{"https://github.com/StillGreen-san/raymino", "MIT"};
-constexpr std::array<LibraryInfo, 5> DEPENDENCY_INFOS{{
-    {"https://github.com/raysan5/raylib", "Zlib"},
-    {"https://github.com/RobLoach/raylib-cpp", "Zlib"},
-    {"https://github.com/raysan5/raygui", "Zlib"},
-    {"https://github.com/raysan5/rres", "MIT"},
-    {"https://github.com/Neargye/magic_enum", "MIT"},
+constexpr LibraryInfo PROJECT_INFO{"https://github.com/StillGreen-san/RogueGlitchTools", "MIT"};
+constexpr std::array<LibraryInfo, 2> DEPENDENCY_INFOS{{
+    {"https://github.com/weidai11/cryptopp", "BSL"},
+    {"https://github.com/catchorg/Catch2", "BSL"},
 }};
 constexpr size_t DEPENDENCY_INFOS_MAX_URL_LEN = std::max_element(DEPENDENCY_INFOS.begin(), DEPENDENCY_INFOS.end(),
     [](const auto& lhs, const auto& rhs)
     {
 	    return lhs.url.size() < rhs.url.size();
-    }) -> url.size();
+    })->url.size();
 } // namespace raymino
