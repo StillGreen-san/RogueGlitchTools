cab281ddd639c0e155ef4800cf65676f56d00eb3
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2023-11-29 12:13:45 +0100

    use convert target

--- src/webui.cpp
+++ src/webui.cpp
@@ -1,18 +1,28 @@
-#include "RogueGlitchDencrypter.hpp"
+#include "convert.hpp"
+#include "crypto.hpp"
+#include "types.hpp"
 
 #ifdef __EMSCRIPTEN__
 #include <emscripten/bind.h>
-#include <emscripten/emscripten.h>
 #else
 #define EMSCRIPTEN_KEEPALIVE
 #endif
 
 #include <string>
+#include <variant>
 #include <vector>
 
 std::vector<unsigned char> tryUpgrade(const std::string& fileContent)
 {
-	return upgrade(reinterpret_cast<const unsigned char*>(fileContent.data()), fileContent.size());
+	std::variant decryptedResult =
+	    rgt::tryDecrypt(reinterpret_cast<const unsigned char*>(fileContent.data()), fileContent.size());
+	if(!std::holds_alternative<rgt::DecryptedSave<rgt::Legacy>>(decryptedResult))
+	{
+		return {};
+	}
+	const rgt::DecryptedSave<rgt::Ultra> upgradedSave =
+	    rgt::upgrade(std::move(std::get<rgt::DecryptedSave<rgt::Legacy>>(decryptedResult)));
+	return rgt::encrypt(upgradedSave);
 }
 
 EMSCRIPTEN_BINDINGS(std)
@@ -20,7 +30,7 @@ EMSCRIPTEN_BINDINGS(std)
 	emscripten::register_vector<unsigned char>("VectorBytes");
 }
 
-EMSCRIPTEN_BINDINGS(RGSU)
+EMSCRIPTEN_BINDINGS(rgt)
 {
 	emscripten::function("tryUpgrade", &tryUpgrade);
 }
