11b9998ceee071165bbfe047e2fbbe5b0013afa1
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2023-11-29 14:20:12 +0100

    change loadFile impl to be compatible with emsdk stdlib

--- src/file.cpp
+++ src/file.cpp
@@ -1,15 +1,19 @@
 #include "file.hpp"
 
+#include <cstddef>
 #include <filesystem>
 #include <fstream>
-#include <iterator>
+#include <ios>
 #include <vector>
 
 namespace rgt
 {
 std::vector<unsigned char> loadFile(const std::filesystem::path& path)
 {
-	std::basic_ifstream<unsigned char> istream(path, std::ios::binary);
-	return {std::istreambuf_iterator{istream}, std::istreambuf_iterator<unsigned char>{}};
+	std::ifstream istream(path, std::ios::in | std::ios::binary);
+	std::vector<unsigned char> fileBuffer;
+	fileBuffer.resize(static_cast<size_t>(std::filesystem::file_size(path)));
+	istream.read(reinterpret_cast<char*>(fileBuffer.data()), static_cast<std::streamsize>(fileBuffer.size()));
+	return fileBuffer;
 }
 } // namespace rgt
