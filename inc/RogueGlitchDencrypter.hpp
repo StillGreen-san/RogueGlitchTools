a700833caf4bdd3502e7d88f5b065a292004cf5b
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2023-11-29 12:15:54 +0100

    remove upgrade target & files

--- inc/RogueGlitchDencrypter.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <string>
-#include <vector>
-
-std::string decrypt(const unsigned char* data, unsigned size);
-std::vector<unsigned char> encrypt(const std::string& inString);
-std::vector<unsigned char> upgrade(const unsigned char* data, unsigned size);
