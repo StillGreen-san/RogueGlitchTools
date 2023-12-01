865992af0c13142fcea8a9fab77481f2826e72aa
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2023-12-01 12:47:35 +0100

    prepare json impl

--- src/convert.cpp
+++ src/convert.cpp
@@ -9,11 +9,53 @@
 
 namespace
 {
+constexpr std::string_view ultraBaseSave = R"({
+	"WinStreak" : { "__type" : "int", "value" : 0 },
+	"UnlockedMaxWorld": { "__type": "int", "value": 1 },
+	"UpgradeCurrency": { "__type": "int", "value": 0 },
+	"UnlockedSkins" : { "__type" : "System.Collections.Generic.List`1[[System.String, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089]],mscorlib",
+		"value" : [ "Default" ] },
+	"UnlockedChars_V2" : { "__type" : "System.Collections.Generic.List`1[[System.String, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089]],mscorlib",
+		"value" : [ "Chap" ] },
+	"UnlockedItems_V2" : { "__type" : "System.Collections.Generic.List`1[[System.String, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089]],mscorlib",
+		"value" : [ ] },
+	"ChallengesDone_V2": { "__type": "System.Collections.Generic.List`1[[System.String, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089]],mscorlib",
+		"value": [ ] },
+	"BabyLevel_V2": { "__type": "int", "value": 0 },
+	"BobLevel_V2": { "__type": "int", "value": 0 },
+	"ChapLevel_V2": { "__type": "int", "value": 0 },
+	"CrocLevel_V2": { "__type": "int", "value": 0 },
+	"DullyLevel_V2": { "__type": "int", "value": 0 },
+	"FailyLevel_V2": { "__type": "int", "value": 0 },
+	"GlitchPixelLevel_V2": { "__type": "int", "value": 0 },
+	"IgorLevel_V2": { "__type": "int", "value": 0 },
+	"LunaLevel_V2": { "__type": "int", "value": 0 },
+	"QuickLevel_V2": { "__type": "int", "value": 0 },
+	"SpiderLevel_V2": { "__type": "int", "value": 0 },
+	"SquibLevel_V2": { "__type": "int", "value": 0 }
+})";
+
 struct ReplacementInfo
 {
 	std::string_view oldThing;
 	std::string_view newThing;
 };
+
+constexpr std::array<ReplacementInfo, 3> itemReplacements{{
+    {"DarkMissiles", "ViralInfection"}, //
+    {"FireRateUpOnCrit", "CritRing"},   //
+    {"SuperJumpBoots", "BoostedJumps"}, //
+}};
+constexpr std::array<ReplacementInfo, 8> challengeRplacements{{
+    {"Boxing Champ", "BoxingChamp"},                //
+    {"Mountain of Corpses", "MountainOfCorpses"},   //
+    {"No Fear!", "NoFear"},                         //
+    {"Out of stock", "OutOfStock"},                 //
+    {"Secret Room", "SecretRoom"},                  //
+    {"Something to cool your anger...", "IceCube"}, //
+    {"Teamwork Needed!", "TeamworkNeeded"},         //
+    {"The Stomper!", "TheStomper"},                 //
+}};
 
 constexpr std::array<ReplacementInfo, 14> replacements{{
     {"System.Int32,mscorlib", "int"},               //
