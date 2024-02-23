2d5c3ae83208a37f6711a6af2fdffe0ac4a4a84d
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2024-02-23 18:04:35 +0100

    add saveFile

--- inc/file.hpp
+++ inc/file.hpp
@@ -6,4 +6,5 @@
 namespace rgt
 {
 std::string loadFile(const std::filesystem::path& path);
+bool saveFile(const std::filesystem::path& path, const std::string& content);
 } // namespace rgt
--- src/file.cpp
+++ src/file.cpp
@@ -15,4 +15,15 @@ std::string loadFile(const std::filesystem::path& path)
 	istream.read(fileBuffer.data(), static_cast<std::streamsize>(fileBuffer.size()));
 	return fileBuffer;
 }
+
+bool saveFile(const std::filesystem::path& path, const std::string& content)
+{
+	std::ofstream ostream(path, std::ios::binary);
+	if(!ostream.is_open())
+	{
+		return false;
+	}
+	ostream.write(content.data(), static_cast<std::streamsize>(content.size()));
+	return true;
+}
 } // namespace rgt
