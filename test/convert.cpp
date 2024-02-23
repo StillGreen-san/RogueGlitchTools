678741009743c9f38016d306d60a21f473e15ed8
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2024-02-23 12:49:01 +0100

    add convert test

---
+++ test/convert.cpp
@@ -0,0 +1,64 @@
+#include "convert.hpp"
+#include "crypto.hpp"
+#include "data.hpp"
+#include "file.hpp"
+#include "types.hpp"
+
+#include <catch2/catch_template_test_macros.hpp>
+#include <catch2/catch_test_macros.hpp>
+
+#include <optional>
+#include <string>
+#include <string_view>
+#include <variant>
+#include <vector>
+
+using namespace rgt;
+
+template<Version TVersion>
+struct Tag;
+template<>
+struct Tag<Ultra>
+{
+	static constexpr Version version = Ultra;
+	static constexpr std::string_view filePath = ULTRA_FILE_PATH;
+};
+template<>
+struct Tag<Legacy>
+{
+	static constexpr Version version = Legacy;
+	static constexpr std::string_view filePath = LEGACY_USED_FILE_PATH;
+};
+
+TEMPLATE_TEST_CASE("convert integration", "", Tag<Legacy>, Tag<Ultra>)
+{
+	constexpr std::string_view expectedKey = "UnlockedItems_V2";
+	constexpr std::string_view expectedItem = "ProjectileShield";
+	constexpr std::string_view unexpectedItem = "FireRateUpOnCrit";
+	constexpr std::string_view unexpectedChallenge = "Something to cool your anger...";
+	constexpr Version TVersion = TestType::version;
+	constexpr std::string_view TFilePath = TestType::filePath;
+
+	const std::vector<unsigned char> file = loadFile(TFilePath);
+
+	const std::variant decryptedResult = tryDecrypt(file.data(), static_cast<unsigned int>(file.size()));
+
+	REQUIRE(std::holds_alternative<DecryptedSave<TVersion>>(decryptedResult));
+
+	const auto& decryptedSave = std::get<DecryptedSave<TVersion>>(decryptedResult);
+
+	const DecryptedSave<Ultra> upgradedSave =
+	    rgt::upgrade({static_cast<std::string>(decryptedSave)}, UpgradeOptionFlags::None);
+
+	REQUIRE(upgradedSave.find(expectedKey) != std::string::npos);
+	REQUIRE(upgradedSave.find(unexpectedItem) == std::string::npos);
+	REQUIRE(upgradedSave.find(unexpectedChallenge) == std::string::npos);
+
+	const DecryptedSave<Ultra> upgradedSaveWithOldDefaults =
+	    rgt::upgrade({static_cast<std::string>(decryptedSave)}, UpgradeOptionFlags::OldDefaults);
+
+	REQUIRE(upgradedSaveWithOldDefaults.find(expectedKey) != std::string::npos);
+	REQUIRE(upgradedSaveWithOldDefaults.find(expectedItem) != std::string::npos);
+	REQUIRE(upgradedSaveWithOldDefaults.find(unexpectedItem) == std::string::npos);
+	REQUIRE(upgradedSaveWithOldDefaults.find(unexpectedChallenge) == std::string::npos);
+}
--- test/data.hpp
+++ test/data.hpp
@@ -3,4 +3,5 @@
 #include <string_view>
 
 constexpr std::string_view LEGACY_FILE_PATH = "SaveData.glitch";
+constexpr std::string_view LEGACY_USED_FILE_PATH = "SaveData.used.glitch";
 constexpr std::string_view ULTRA_FILE_PATH = "SaveData__Deluxe.glitch";
